a53e3256af91820dd1a6b0dc5e0eaefb25b8d700
skeeo8221 <skeeo8221@HRH-IC0027318.wrdsb.ca>
2017-09-19 13:57:18 -0400

    added Q5

---
+++ src/Assignment_1/A1Q5.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Assignment_1;
+//text
+/**
+ *
+ * @author skeeo8221
+ */
+public class A1Q5 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
