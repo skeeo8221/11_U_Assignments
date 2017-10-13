8373e6e7ee5e2bb27ab1d469893d5d1b754bd348
skeeo8221 <skeeo8221@HRH-IC0027322.wrdsb.ca>
2017-10-13 14:23:22 -0400

    quiz_#1

--- src/quizes/quiz_1.java
+++ src/quizes/quiz_1.java
@@ -15,7 +15,8 @@ import becker.robots.Wall;
  * @author skeeo8221
  */
 public class quiz_1 {
-   /**
+
+    /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
@@ -33,14 +34,14 @@ public class quiz_1 {
         Robot sunsar = new Robot(Sunsartown, 1, 1, Direction.EAST);
 
 
-        //create 10 things
+        //create 3 things
         Thing a = new Thing(Sunsartown, 1, 3);
         Thing b = new Thing(Sunsartown, 1, 4);
         Thing c = new Thing(Sunsartown, 1, 5);
-       
 
-        
-         //create walls
+
+
+        //create walls
         new Wall(Sunsartown, 1, 3, Direction.NORTH);
         new Wall(Sunsartown, 1, 4, Direction.NORTH);
         new Wall(Sunsartown, 1, 5, Direction.NORTH);
@@ -49,76 +50,81 @@ public class quiz_1 {
         new Wall(Sunsartown, 1, 5, Direction.SOUTH);
         new Wall(Sunsartown, 1, 5, Direction.EAST);
         new Wall(Sunsartown, 1, 1, Direction.WEST);
-        
-       
+
+
         //new variable that counts things picked up
         int thingCount = 0;
 
-        
-        
+
+
         //while loop if less than 10 things have been moved
         while (true) {
-            
-            
-            
-         while (thingCount < 3) {
-            
-             if (sunsar.frontIsClear()) {
 
-             if (sunsar.canPickThing()){
-                 
-                 
-                 sunsar.pickThing();
-                 sunsar.turnLeft();
-                 sunsar.turnLeft();
-              
-                 
-                 thingCount = thingCount + 1;
-                 
-                 
-             }else{
-                 
 
-                      sunsar.move();
-                 
-             }
-             }
-          if (sunsar.frontIsClear()) {
-              
-              
-          }else{ 
-              
-             
-                      
-                  sunsar.turnLeft();
-                  sunsar.turnLeft();
-                  
-                  sunsar.getDirection();
-                          
-                  if (sunsar.getDirection() == Direction.EAST) {
-                
-                     sunsar.putThing();
-                     sunsar.move();
-                 }
+//while less than 3 things have been moved out of the mine, take them out
+            while (thingCount < 3) {
+
+                if (sunsar.frontIsClear()) {
+
+                    if (sunsar.canPickThing()) {
+
+
+                        sunsar.pickThing();
+                        sunsar.turnLeft();
+                        sunsar.turnLeft();
+
+                        if (thingCount == 3) {
+
+                            sunsar.turnLeft();
+                            sunsar.turnLeft();
+
+                            if (sunsar.frontIsClear()) {
+
+                                sunsar.move();
+
+                            }
+
+                            sunsar.turnLeft();
+                            sunsar.turnLeft();
+
+                            sunsar.putThing();
+
+
+                            break;
+
+                        }
+
+                    } else {
+
+
+                        sunsar.move();
+
+                    }
+
+
+                }
+                if (sunsar.frontIsClear()) {
+                } else {
+
+
+
+                    // return and drop thing
+                    sunsar.turnLeft();
+                    sunsar.turnLeft();
+
+                    sunsar.getDirection();
+
+                    if (sunsar.getDirection() == Direction.EAST) {
+
+                        sunsar.putThing();
+                        sunsar.move();
+                    }
+                }
+
+            }
+
+
+
         }
-
     }
-
-        
-                sunsar.turnLeft();
-                sunsar.turnLeft();
-                
-                 sunsar.move();
-                  sunsar.move();
-                   sunsar.move();
-                  sunsar.move();
-                  
-                  sunsar.turnLeft();
-                sunsar.turnLeft();
-                
-                
-                  break;
-        }
-}
-}
 }
