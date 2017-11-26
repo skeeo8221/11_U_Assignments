7a37a92b9eb7c4b1c286c412105abe5d5a2d389a
owen <owen@192.168.0.20>
2017-11-26 16:42:21 -0500

    finished A6Q4

--- src/Assignment_6/A6Q4.java
+++ src/Assignment_6/A6Q4.java
@@ -18,45 +18,52 @@ public class A6Q4 {
     public static void main(String[] args) {
         // TODO code application logic here
 
-
-
-
         //new variable to hold the place of an integer when swaping places
         int placeHolder = 0;
 
         //a new integer array to store the two numbers
         int[] numbers = new int[10];
 
-
         //print "enter the value of the two numbers"
         System.out.println("enter the value of the ten marks");
 
-           
-            //use a loop to get all the info
-            for (int i = 0; i < numbers.length; i++) {
+        //use a loop to get all the info
+        for (int i = 0; i < numbers.length; i++) {
 
-                //create scanner to use as input
-                Scanner input = new Scanner(System.in);
+            //create scanner to use as input
+            Scanner input = new Scanner(System.in);
 
-                //the array input is the next line
-                numbers[i] = input.nextInt();
+            //the array input is the next line
+            numbers[i] = input.nextInt();
 
-            }
-
-
-              //use the following statements to sort the 10 marks into the right order
-            for (int i = 0; i < numbers.length; i++) {
-
-            for () {
-                
-            if (numbers[0] > numbers[1]) {
-                
-                
-                
-            }
-            
-    
         }
+
+        //use the following statements to sort the 10 marks into the right order
+        for (int x = 0; x < numbers.length - 1; x++) {
+
+            for (int y = 0; y < numbers.length - x - 1; y++) {
+
+                if (numbers[y] > numbers[y + 1]) {
+
+                    //swap the places in the array of the integers using the placeholder variable
+                    placeHolder = numbers[y];
+
+                    numbers[y] = numbers[y + 1];
+
+                    numbers[y + 1] = placeHolder;
+
+                }
+            }
+        }
+
+        System.out.println("values1");
+        for (int x = 0; x < numbers.length; x++) {
+
+            //print the two numbers in ascending order
+            System.out.println(numbers[x]);
+
+        }
+
     }
-    }
+
 }
