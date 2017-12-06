70d6c58601706a62d07ee880d5822dc1bcec65c6
skeeo8221 <skeeo8221@HRH-IC0027322.wrdsb.ca>
2017-12-06 13:13:48 -0500

    about to start #3

--- src/Assignment_7/A7Q2.java
+++ src/Assignment_7/A7Q2.java
@@ -15,36 +15,51 @@ public class A7Q2 {
     /**
      * @param args the command line arguments
      */
-    //double return type means it needs to send answer
-    public double areaOfCircle(double radius) {
+    //double void type means it does not need to send answer
+    public void examGrade(double examMark) {
 
-        //do equation to determine area from radius
-        double answer = radius * radius * Math.PI;
 
-        //send back the answer
-        return answer;
-        
+        // sort the marks into the letter grades that they belong in
+        if (examMark < 50) {
+            //print the letter mark
+            System.out.println("letter grade from yor exam is F ");
+        }
+        if (examMark < 59 && examMark > 50) {
+            //print the letter mark
+            System.out.println("letter grade from yor exam is D ");
+        }
+        if (examMark < 69 && examMark > 60) {
+            //print the letter mark
+            System.out.println("letter grade from yor exam is C ");
+        }
+        if (examMark < 79 && examMark > 70) {
+            //print the letter mark
+            System.out.println("letter grade from yor exam is B ");
+        }
+        if (examMark > 80) {
+            //print the letter mark
+            System.out.println("letter grade from yor exam is A ");
+        }
     }
 
     public static void main(String[] args) {
-        
-        
-        A7Q1 test = new A7Q1();
 
-        // ask for the radius of the circle
-        System.out.println("please enter the radius of the circle");
+
+        A7Q2 test = new A7Q2();
+
+        // ask for the percentage grade
+        System.out.println("please enter the value of the grade you got on your exam");
 
         //new scanner for input
         Scanner in = new Scanner(System.in);
 
-        //new double called radius
-        double radius = in.nextDouble();
+        //new double called examMark
+        double examMark = in.nextDouble();
 
-        //new double area of circle uses the radius
-        double areaOfCircle = test.areaOfCircle(radius);
+        //new double examGrade uses the examMark
+        test.examGrade(examMark);
 
-        //print the area of the circle
-        System.out.println("the area of the circle is " + areaOfCircle);
+
 
 
 
